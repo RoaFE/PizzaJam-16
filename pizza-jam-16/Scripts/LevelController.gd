23a136fa68b5a74e4da02459c80fd7e2825f45ce
RoaFE <R.stockton06@gmail.com>
2026-03-18 21:30:17 +0000

    Simple way of switching from menu to level and back

--- pizza-jam-16/Scripts/LevelController.gd
+++ pizza-jam-16/Scripts/LevelController.gd
@@ -1,3 +1,15 @@
 extends Node
 
 @export var _objectiveManager : ObjectiveManager
+@export var menu : PackedScene
+
+func _ready() -> void:
+	_objectiveManager.AllObjectivesComplete.connect(LevelCompleted)
+
+func LoadMenu():
+	var scene = menu.instantiate()
+	get_tree().root.add_child(scene)
+	queue_free()
+
+func LevelCompleted():
+	LoadMenu()
--- pizza-jam-16/Scripts/Menu.gd
+++ pizza-jam-16/Scripts/Menu.gd
@@ -1,1 +1,11 @@
 extends Node
+
+@export var level : PackedScene
+
+func LoadLevel():
+	var scene = level.instantiate()
+	get_tree().root.add_child(scene)
+	queue_free()
+	
+func Quit():
+	get_tree().quit()
